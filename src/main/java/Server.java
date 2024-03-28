cc1acd681eaf97abae1c940c7543ecb0c69904e3
alexdang02 <alexdang1602@gmail.com>
2024-03-28 19:53:31 +1100

    Add loading dictionary code

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,13 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.lang.NullPointerException;
 
 
 public class Server {
+
     public static void main(String[] args) {
 
         // validate use inputs
@@ -14,6 +20,9 @@ public class Server {
 
         System.out.println(port);
         System.out.println(dictionaryFile);
+
+        // load sample dictionary data into ConcurrentHashMap
+        ConcurrentHashMap<String, String> map = loadDictionaryIntoMap(dictionaryFile);
     }
 
     private static boolean validateArgs(String[] args) {
@@ -44,5 +53,43 @@ public class Server {
         }
 
         return true;
+    }
+
+    private static ConcurrentHashMap<String, String> loadDictionaryIntoMap(String fileName) {
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        int nullEntryCount = 0;
+        int totalEntryCount = 0;
+
+        //
+        try {
+            JsonNode rootNode = mapper.readTree(new File(fileName));
+            if (rootNode.isArray()) {
+                totalEntryCount = rootNode.size();
+                for (JsonNode arrayElement : rootNode) {
+
+                        try {
+                            // Extract the word and meaning from the current JSON object.
+                            String word = arrayElement.get("word").asText();
+                            String meaning = arrayElement.get("description").asText();
+
+                            // Extract the word and meaning from the current JSON object.
+                            if (word.isEmpty() || meaning.isEmpty()){
+                                throw new NullPointerException();
+                            }
+                            map.put(word, meaning);
+                             // System.out.println(STR."Entry\{word}-\{meaning}");
+                        }catch (NullPointerException e){
+                            nullEntryCount += 1;
+                        }
+                    }
+                }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            System.out.println(STR."Invalid entry count: \{nullEntryCount}");
+            System.out.println(STR."Valid entry count: \{totalEntryCount - nullEntryCount}");
+        }
+        return map;
     }
 }
