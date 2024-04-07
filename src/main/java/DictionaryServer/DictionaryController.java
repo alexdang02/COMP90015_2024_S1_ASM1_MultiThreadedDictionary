4f2d962e4532ae473cc1a7e0b78d3e05dc49b2d2
alexdang02 <alexdang1602@gmail.com>
2024-04-08 05:29:30 +1000

    Adding requesttype to reply

--- src/main/java/DictionaryServer/DictionaryController.java
+++ src/main/java/DictionaryServer/DictionaryController.java
@@ -18,7 +18,7 @@ public class DictionaryController {
         this.service = service;
     }
 
-    public ClientReply requestHandler(ClientRequest request) {
+    public ServerReply requestHandler(ClientRequest request) {
         String word = request.word;
         String definition = request.definition;
         String replyData;
@@ -26,23 +26,23 @@ public class DictionaryController {
         return switch (request.getRequestType()) {
             case SEARCH:
                 replyData = service.getDefinition(word);
-                yield new ClientReply(replyData != null ? ReplyCode.FOUND : ReplyCode.NOT_FOUND, replyData);
+                yield new ServerReply(RequestType.SEARCH, replyData != null ? ReplyCode.FOUND : ReplyCode.NOT_FOUND, replyData);
 
             case ADD:
                 replyData = service.addWord(word, definition);
-                yield new ClientReply(replyData == null ? ReplyCode.CREATED : ReplyCode.CONFLICT, replyData);
+                yield new ServerReply(RequestType.ADD, replyData != null ? ReplyCode.CONFLICT : ReplyCode.CREATED, replyData);
 
             case DELETE:
                 replyData = service.removeWord(word);
-                yield new ClientReply(replyData != null ? ReplyCode.OK : ReplyCode.NOT_FOUND, replyData);
+                yield new ServerReply(RequestType.DELETE, replyData != null ? ReplyCode.OK : ReplyCode.NOT_FOUND, replyData);
 
             case UPDATE:
                 replyData = service.updateWord(word, definition);
-                yield new ClientReply(replyData != null ? ReplyCode.OK : ReplyCode.NOT_FOUND, replyData);
+                yield new ServerReply(RequestType.UPDATE, replyData != null ? ReplyCode.OK : ReplyCode.NOT_FOUND, replyData);
             case ERROR:
                 // NOTE: In case of Bad Request, word component in ClientRequest will be used as explanation returned to users
                 // Since error message is stored in word, pass it on to user
-                yield new ClientReply(ReplyCode.BAD_REQUEST, word);
+                yield new ServerReply(RequestType.ERROR, ReplyCode.BAD_REQUEST, word);
         };
 
 
